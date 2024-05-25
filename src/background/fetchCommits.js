fa1bf7fe81cb593829c9164d91fd559fd12d7f71
Klement Omeri <klement.omeri@ext.trustyou.net>
2024-05-26 00:21:06 +0200

    refactor: move background to src/

---
+++ src/background/fetchCommits.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const {User, Commit} = require('../models'); // Update Commit model to use Sequelize
+
+const fetchCommits = async () => {
+  const users = await User.findAll();
+  users.forEach(async (user) => {
+    const repos = await axios.get('https://api.github.com/user/starred', {
+      headers: {'Authorization': `token ${user.accessToken}`}
+    });
+    repos.data.forEach(async (repo) => {
+      const commits = await axios.get(`https://api.github.com/repos/${repo.owner.login}/${repo.name}/commits`, {
+        headers: {'Authorization': `token ${user.accessToken}`}
+      });
+      commits.data.forEach(async (commit) => {
+        await Commit.create({
+          userId: user.id,
+          repoName: repo.name,
+          commitDate: commit.commit.author.date,
+        });
+      });
+    });
+  });
+};
+
+module.exports = fetchCommits;
