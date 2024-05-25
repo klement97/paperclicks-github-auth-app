e74d6232bf1d9c94474d665b8b3b91aeb29b5eb9
Klement Omeri <klement.omeri@ext.trustyou.net>
2024-05-26 00:24:40 +0200

    feat: add user migration

---
+++ migrations/20240525192326-create-user.js
@@ -0,0 +1,48 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      githubId: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      displayName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      accessToken: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
