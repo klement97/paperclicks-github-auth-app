80a8ba8e6fed3fbbe206db4589ebfaf9b7303f79
klement97 <klementomeri97@gmail.com>
2024-05-29 12:11:55 +0300

    feat: configure scheduler for commit fetching

---
+++ src/schedule.js
@@ -0,0 +1,13 @@
+const cron = require('node-cron');
+const fetchCommits = require('./background/fetchCommits');
+
+console.log("Cron job registered and scheduled.")
+// Schedule the task to run every day at midnight
+cron.schedule('* * * * *', () => {
+    console.log('Fetching commits...');
+    fetchCommits().then(() => {
+        console.log('Commits data fetched and stored successfully');
+    }).catch((error) => {
+        console.error(`Failed to fetch commits: ${error.message}`);
+    });
+});
